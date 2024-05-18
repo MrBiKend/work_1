a53629e97037ccb42fae3231a7a0e572b40ac9b0
MrBiKend <bikend.learn@gmail.com>
2024-05-18 09:39:20 +0400

    Теперь после ввода значений с клавиатуры программа автоматически выведет информацию о дороге, что делает ее более удобной для использования.

--- ex2.cpp
+++ ex2.cpp
@@ -1,1 +1,40 @@
-Practika1.2
+#include <iostream>
+
+class Road {
+private:
+  int length;
+  int lanes;
+
+public:
+  Road() {}
+
+  void setValuesFromInput() {
+    std::cout << "Введите длину дороги (в метрах): ";
+    std::cin >> length;
+
+    std::cout << "Введите количество полос: ";
+    std::cin >> lanes;
+  }
+
+  int getLength() {
+    return length;
+  }
+
+  int getLanes() {
+    return lanes;
+  }
+
+  // Новый метод для вывода информации о дороге
+  void printInfo() {
+    std::cout << "Информация о дороге:" << std::endl;
+    std::cout << "Длина: " << getLength() << " метров" << std::endl;
+    std::cout << "Полосы: " << getLanes() << std::endl;
+  }
+};
+
+int main() {
+  Road road;
+  road.setValuesFromInput();
+  road.printInfo();
+  return 0;
+}
